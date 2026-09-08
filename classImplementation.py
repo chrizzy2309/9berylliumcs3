7aaa2216b8b0aa40097540be1a405bc3c5200458
chrizzy2309 <cdbbaldoza@brc.pshs.edu.ph>
2026-09-08 23:07:09 +0800

    Implement display methods for GENSHIN class
    Added display methods for character attributes and updated character instantiation with example characters.

--- classImplementation.py
+++ classImplementation.py
@@ -5,28 +5,129 @@ class GENSHIN:
         self.element = element
         self.weapon = weapon
 
-self.name = input("Enter character name: ")
-self.nation = input("Enter character nation: ")
-self.element = input("Enter character element: ")
-self.weapon = input("Enter character weapon: ")
 
-def
-character = GENSHIN(self.name, self.nation, self.element, self.weaponM)
-print(character.name)
+    def display_name(self):
+        print(f"{self.name})")
 
+    def display_nation(self):
+        print(f"{self.nation}")
 
-class GENSHIN:
-    def __init__(self, name, nation, element, weapon):
-        self.name = name
-        self.nation = nation
-        self.element = element
-        self.weapon = weapon
+    def display_element(self):
+        print(f"{self.element}")
 
-inventory = []
-def choices():
-    print("1. ADD")
-    print("2. REMOVE")
-    print("3. ADD")
-    
-    choice = input("Choose a funcion: ")
-    if choice == 1:
+    def display_weapon(self):
+        print(f"{self.weapon}")
+
+character79 = GENSHIN("Arlechinno", "Fontaine", "Pyro", "Polearm")
+character110 = GENSHIN("Zibai", "Liyue", "Geo", "Sword") 
+
+print("Initial Character Information")
+
+print()
+
+print("For character79")
+
+print()
+
+print("Character Name: ")
+character79.display_name()
+
+print()
+
+print("Character Nation: ")
+character79.display_nation()
+
+print()
+
+print("Character Element: ")
+character79.display_element()
+
+print()
+
+print("Character Weapon: ")
+character79.display_weapon()
+
+print()
+
+print("For character110")
+
+print()
+
+print("Character Name: ")
+
+character110.display_name()
+
+print()
+
+print("Character Nation: ")
+
+character110.display_nation()
+
+print()
+
+print("Character Element: ")
+
+character110.display_element()
+
+print()
+
+print("Character Weapon: ")
+character110.display_weapon()
+
+print()
+
+setattr(character79, "weapon", "Catalyst")
+
+print()
+print()
+
+print("Updated Character Information: ")
+
+print()
+
+print("For character79")
+
+print()
+
+print("Character Name: ")
+character79.display_name()
+
+print()
+
+print("Character Nation: ")
+character79.display_nation()
+
+print()
+
+print("Character Element: ")
+character79.display_element()
+
+print()
+
+print("Character Weapon: ")
+character79.display_weapon()
+
+print()
+print()
+
+print("For character110")
+
+print()
+
+print("Character Name: ")
+character110.display_name()
+
+print()
+
+print("Character Nation: ")
+character110.display_nation()
+
+print()
+
+print("Character Element: ")
+character110.display_element()
+
+print()
+
+print("Character Weapon: ")
+character110.display_weapon()
