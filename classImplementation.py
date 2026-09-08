8afc5e0dc65fd805cd135910f73b6b252e634978
chrizzy2309 <cdbbaldoza@brc.pshs.edu.ph>
2026-09-08 13:17:42 +0800

    Update classImplementation.py

--- classImplementation.py
+++ classImplementation.py
@@ -13,3 +13,20 @@ self.weapon = input("Enter character weapon: ")
 def
 character = GENSHIN(self.name, self.nation, self.element, self.weaponM)
 print(character.name)
+
+
+class GENSHIN:
+    def __init__(self, name, nation, element, weapon):
+        self.name = name
+        self.nation = nation
+        self.element = element
+        self.weapon = weapon
+
+inventory = []
+def choices():
+    print("1. ADD")
+    print("2. REMOVE")
+    print("3. ADD")
+    
+    choice = input("Choose a funcion: ")
+    if choice == 1:
