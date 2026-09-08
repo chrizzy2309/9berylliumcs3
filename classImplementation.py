63e69f41d41f4eb021a0bf92f1d3c4faeb5a9819
chrizzy2309 <cdbbaldoza@brc.pshs.edu.ph>
2026-09-08 12:09:22 +0800

    Update classImplementation.py

--- classImplementation.py
+++ classImplementation.py
@@ -1,1 +1,15 @@
+class GENSHIN:
+    def __init__(self, name, nation, element, weapon):
+        self.name = name
+        self.nation = nation
+        self.element = element
+        self.weapon = weapon
 
+self.name = input("Enter character name: ")
+self.nation = input("Enter character nation: ")
+self.element = input("Enter character element: ")
+self.weapon = input("Enter character weapon: ")
+
+def
+character = GENSHIN(self.name, self.nation, self.element, self.weaponM)
+print(character.name)
