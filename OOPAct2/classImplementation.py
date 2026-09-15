edeef977694981f6814aac8197b2290bf0db912b
chrizzy2309 <cdbbaldoza@brc.pshs.edu.ph>
2026-09-15 13:19:06 +0800

    Update classImplementation.py

--- OOPAct2/classImplementation.py
+++ OOPAct2/classImplementation.py
@@ -1,25 +1,4 @@
-class GENSHIN:
-    def __init__(self, name, nation, element, weapon):
-        self.name = name
-        self.nation = nation
-        self.element = element
-        self.weapon = weapon
-
-
-    def display_name(self):
-        print(f"{self.name})")
-
-    def display_nation(self):
-        print(f"{self.nation}")
-
-    def display_element(self):
-        print(f"{self.element}")
-
-    def display_weapon(self):
-        print(f"{self.weapon}")
-
 character79 = GENSHIN("Arlechinno", "Fontaine", "Pyro", "Polearm")
-character110 = GENSHIN("Zibai", "Liyue", "Geo", "Sword") 
 
 print("Initial Character Information")
 
@@ -30,6 +9,7 @@ print("For character79")
 print()
 
 print("Character Name: ")
+add 
 character79.display_name()
 
 print()
@@ -48,32 +28,6 @@ print("Character Weapon: ")
 character79.display_weapon()
 
 print()
-
-print("For character110")
-
-print()
-
-print("Character Name: ")
-
-character110.display_name()
-
-print()
-
-print("Character Nation: ")
-
-character110.display_nation()
-
-print()
-
-print("Character Element: ")
-
-character110.display_element()
-
-print()
-
-print("Character Weapon: ")
-character110.display_weapon()
-
 print()
 
 setattr(character79, "weapon", "Catalyst")
@@ -110,24 +64,29 @@ character79.display_weapon()
 print()
 print()
 
-print("For character110")
 
-print()
+class NATION:
+    def __init__(self, name,  weapon, element, region):
+        
+        self.name = name
+        self.weapon = weapon
+        self.element = element
+        self.region = region
+       
 
-print("Character Name: ")
-character110.display_name()
+    def display_name(self):
+        print(f"{self.name}")
+    
+    def display_weapon(self):
+        print(f"{self.weapon}")
+        
+    def display_element(self):
+        print(f"{self.element}")
+        
+    def display_region(self):
+        print(f"{self.region}")
+        
+NATION.
+        
 
-print()
 
-print("Character Nation: ")
-character110.display_nation()
-
-print()
-
-print("Character Element: ")
-character110.display_element()
-
-print()
-
-print("Character Weapon: ")
-character110.display_weapon()
